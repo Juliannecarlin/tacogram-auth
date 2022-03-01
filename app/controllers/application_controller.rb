90aab17567b6a58719b449aa9ba9bf66fbecb62c
Juliannecarlin <julianne.carlin@kellogg.northwestern.edu>
2022-03-01 13:50:34 +0000

    Save1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :current_user
 
   def current_user
-    puts "------------------ code before every request ------------------"
+    # puts "------------------ code before every request ------------------"
     # TODO: @current_user = ...
+    @current_user = User.find(session[:user_id]) if session[:uder_id]
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,5 +4,20 @@ class SessionsController < ApplicationController
   
   def create
     # TODO: authenticate user
+    @user = User.find_by({ email: params["email"] })
+    if @user  
+      if BCrypt::Password.new(@user.password) == params["password"]
+        session[:used_id] = @user.id 
+        flash[:notice] = "Welcome, #{@user.first_name}."
+        redirect_to "/posts"
+      else
+        flash[:notice] = "Nope"
+        redirect_to "/sessions/new"
+      end
+    else  
+      flash[:notice] = "Nope"
+      redirect_to "/sessions/new"
+    end
+   end
   end
 end
